318e3ff094e5046e59fcbdbfc3fd234915ab5b0e
Максим Щапов <maksimsapov0456@gmail.com>
2024-12-03 21:08:58 +0500

    Task5

--- Tyuiu.SchcapovMA.Sprint5.Task5.V28.Lib/DataService.cs
+++ Tyuiu.SchcapovMA.Sprint5.Task5.V28.Lib/DataService.cs
@@ -5,26 +5,26 @@ namespace Tyuiu.SchcapovMA.Sprint5.Task5.V28.Lib
     public class DataService : ISprint5Task5V28
     {
         public double LoadFromDataFile(string path)
+            
+
         {
             int min = 1000;
             double res = 0;
             int fact = 1;
             using (StreamReader reader = new StreamReader(path))
             {
-                string? line;
+                string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     string lineReplace = line.Replace('.', ',');
                     string[] lineArray = lineReplace.Split(' ');
-
                     foreach (string number in lineArray)
                     {
                         double lineParse = double.Parse(number);
-
-                        if ((lineParse > 0) && (lineParse % 5 == 0))
+                        if ((lineParse > 0) && (lineParse % 5 == 0)) 
                         {
                             res = lineParse;
-                            while (res < min)
+                            if (res < min)
                             {
                                 min = (int)res;
                                 for (int i = 1; i <= min; i++)
@@ -37,7 +37,6 @@ namespace Tyuiu.SchcapovMA.Sprint5.Task5.V28.Lib
                 }
             }
             return fact;
-            
         }
     }
 }
